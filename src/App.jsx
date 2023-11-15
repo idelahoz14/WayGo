79d71af4f9b89cdda0b8bddba15273150b502fff
idelahoz14 <idelahoz14@cuc.edu.co>
2023-11-15 11:50:50 -0500

    Adding new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <>
-      <h2>Hola mundo</h2>
+      <NavBar title='Hola Mundo' />
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = ({title}) => {
+  return(
+    <>
+      <h2>{title}</h2>
+    </>
+  )
+}
+
+export default NavBar;
